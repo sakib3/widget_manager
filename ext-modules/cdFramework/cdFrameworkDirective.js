c560238ec1955dc35304de682b22f04ba69e5726
unknown <sakib3@gmail.com>
2016-02-21 00:19:19 +0100

    passing values to an isolate scope

--- ext-modules/cdFramework/cdFrameworkDirective.js
+++ ext-modules/cdFramework/cdFrameworkDirective.js
@@ -6,7 +6,9 @@ angular.module("cdFramework")
         return {
             transclude: true,
             scope: {
-            
+                title: '@',
+                subtitle: '@',
+                iconFile: '@'
             },
             controller: "cdFrameworkController",
             templateUrl: "ext-modules/cdFramework/cdFrameworkTemplate.html"
