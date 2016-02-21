f0f89a639c84c339e0288da81401c0c2209d7d04
unknown <sakib3@gmail.com>
2016-02-21 22:38:09 +0100

    a menu item custom directive

--- ext-modules/cdMenu/cdMenuDirective.js
+++ ext-modules/cdMenu/cdMenuDirective.js
@@ -1,10 +1,13 @@
 ﻿
-"use restrict"
+"use restrict";
 
 angular.module("cdMenu")
        .directive("cdMenu", function () {
            return {
                transclude: true,
+               scope: {
+
+               },
                controller: "cdMenuController",
                templateUrl: "ext-modules/cdMenu/cdMenuTemplate.html",
                link: function (scope, el, attr) {
---
+++ ext-modules/cdMenu/cdMenuItemDirective.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="C:\Users\MdSabbirRahman\Desktop\widget_manager\index.html" />
+
+"use restrict";
+angular.module("cdMenu")
+       .directive("cdMenuItemDirective", function () {
+           return {
+               //require has a parent cdMenu
+               require: "^cdMenu",
+               scope: {
+                   label: '@'
+               },
+               templateUrl: "ext-modules/cdMenu/cdMenuItemTemplate.html",
+               link: function (scope, el, attr,ctrl) {
+
+               }
+
+           };
+
+       }
+);
